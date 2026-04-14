b64de902c9f6324feda60e3c82cc6e318ab1599b
ridhisingh06 <singhridhi60803@gmail.com>
2026-04-14 15:51:06 +0530

    Frontend setup with auth, dashboard, inventory modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('invmgmt-frontend');
+export class AppComponent {
+  title = signal('invmgmt-frontend');
 }
