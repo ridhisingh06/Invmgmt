b50ed0de1942e7e15f614fa3b8f9296d7e1532a2
ridhisingh06 <singhridhi60803@gmail.com>
2026-06-07 13:02:53 +0530

    remove all markdown files

--- invmgmt.web/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using invmgmt.web.DTOs;
-
-namespace invmgmt.web.Services
-{
-    public interface IAuthService
-    {
-        Task<(bool Success, string Token, string Message)> LoginAsync(LoginRequest dto);
-    }
-}
